d99711c29af6ce222fceddc80364e4d980f2a0b1
ashutoshasthana <aaashutoshasthana9@gmail.com>
2020-05-13 21:59:46 +0530

    Changes merged

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  agora: {
+    appId: '08c6b5a8975b496da600711633959335'
+  }
 };
 
 /*
